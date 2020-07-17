2eb3715afd9a9336c56045f7d36b6957d86094bd
siewyanhoh <siew.yan.hoh@cern.ch>
2020-07-17 06:20:57 +0200

    update

--- solution.py
+++ solution.py
@@ -8,8 +8,9 @@ def predict(data):
     """ get two colums from `df` and returns the predicted ouput as a column (pd.Series)
     TODO: modify it
     """
-    result = data['col1'] - data['col2']
-    return result
+    
+    data['predicted'] = data['col1'] - data['col2']
+    return data
 
 
 def main(input_dir, output_dir):
@@ -17,15 +18,16 @@ def main(input_dir, output_dir):
     df = pd.read_csv(os.path.join(input_dir, 'data.tsv'), sep='\t')
 
     predicted_result = predict(df)
-    assert isinstance(predicted_result, pd.core.series.Series), \
-        f"Invalid predicted output type {type(predicted_result)}"
+    #assert isinstance(predicted_result, pd.core.series.Series), \
+    #    print("Invalid predicted output type {type(predicted_result)}")
 
     predicted_result.to_csv(
         os.path.join(output_dir, 'data.predict'),
         index=False,
-        header=False)
+        sep='\t',
+        header=True)
 
 
 if __name__ == "__main__":
-    assert len(sys.argv) >= 3, f"Invalid number of arguments: {len(sys.argv)}"
+    assert len(sys.argv) >= 3, print("Invalid number of arguments: {len(sys.argv)}")
     main(sys.argv[1], sys.argv[2])
